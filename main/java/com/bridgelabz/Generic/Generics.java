80a2d9668d78b1cf9f5797fa4244957242f38bd8
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-11 11:44:25 +0530

    ADD:Generic Method added

--- main/java/com/bridgelabz/Generic/Generics.java
+++ main/java/com/bridgelabz/Generic/Generics.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.Generic;
 
 public class Generics {
-	public Integer findMax(Integer value1, Integer value2, Integer value3) {
-		Integer max = value1;
+	public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+		T max = value1;
 		if (value2.compareTo(max) > 0) {
 			max = value2;
 		}
@@ -13,30 +13,7 @@ public class Generics {
 		return max;
 	}
 
-	// Find float maximum value
-	public Float findMax(Float value1, Float value2, Float value3) {
-		Float max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum Float number is " + max);
-		return max;
-	}
+	
 
-	// Find String maximum value
-	public String findMax(String value1, String value2, String value3) {
-		String max = value1;
-		if (value2.compareTo(max) > 0) {
-			max = value2;
-		}
-		if (value3.compareTo(max) > 0) {
-			max = value3;
-		}
-		System.out.println("Maximum String number is " + max);
-		return max;
-	}
 
 }
