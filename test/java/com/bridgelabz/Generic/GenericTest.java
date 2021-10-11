8822f521e14c9579cfe890b0b4c35bcfab9692f3
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-11 11:36:06 +0530

    ADD:Maximum float value found

--- test/java/com/bridgelabz/Generic/GenericTest.java
+++ test/java/com/bridgelabz/Generic/GenericTest.java
@@ -11,23 +11,42 @@ public class GenericTest
 {
 Generics max=new Generics();
 	
-	@Test
-	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
-		int value = max.findMax(9, 5, 6);
-		Assert.assertEquals(9, value);
-	}
+@Test
+public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
+	int value = max.findMax(9, 5, 6);
+	Assert.assertEquals(9, value);
+}
 
-	@Test
-	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
-		int value = max.findMax(5, 9, 6);
-		Assert.assertEquals(9, value);
-	}
+@Test
+public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
+	int value = max.findMax(5, 9, 6);
+	Assert.assertEquals(9, value);
+}
 
-	@Test
-	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
-		int value = max.findMax(6, 5, 9);
-		Assert.assertEquals(9, value);
-	}
+@Test
+public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
+	int value = max.findMax(6, 5, 9);
+	Assert.assertEquals(9, value);
+}
+
+@Test
+public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
+	float value = max.findMax(9.0f, 5.0f, 6.0f);
+	Assert.assertEquals(9.0f, value, 0.0f);
+}
+
+@Test
+public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
+	float value = max.findMax(5.0f, 9.0f, 6.0f);
+	Assert.assertEquals(9.0f, value, 0.0f);
+}
+
+@Test
+public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
+	float value = max.findMax(9.0f, 5.0f, 11.0f);
+	Assert.assertEquals(11.0f, value, 0.0f);
+}
+
 
  
 }
