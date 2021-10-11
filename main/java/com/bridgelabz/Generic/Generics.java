853959fffd2adb82ce221bf8aafa5f2e9f54ba3c
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-11 11:53:44 +0530

    ADD:Generic Class Created

--- main/java/com/bridgelabz/Generic/Generics.java
+++ main/java/com/bridgelabz/Generic/Generics.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.Generic;
 
-public class Generics {
-	public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+public class Generics <T extends Comparable<T>> {
+
+	T value1, value2, value3;
+
+	public Generics(T value1, T value2, T value3) {
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
+
+	public static <T extends Comparable<T>> T testMaximum(T value1, T value2, T value3) {
 		T max = value1;
 		if (value2.compareTo(max) > 0) {
 			max = value2;
@@ -9,11 +18,17 @@ public class Generics {
 		if (value3.compareTo(max) > 0) {
 			max = value3;
 		}
-		System.out.println("Maximum Integer number is " + max);
+		System.out.println("Maximum value is " + max);
 		return max;
 	}
+
+	public T findMax() {
+		return Generics.testMaximum(value1, value2, value3);
+	}
+
+}
 
 	
 
 
-}
+
--- test/java/com/bridgelabz/Generic/GenericTest.java
+++ test/java/com/bridgelabz/Generic/GenericTest.java
@@ -1,70 +1,64 @@
 package com.bridgelabz.Generic;
 
-
-
-
 import org.junit.Test;
+import org.junit.Assert;
 
-import junit.framework.Assert;
 
-public class GenericTest  
-{
-Generics max=new Generics();
+public class GenericTest {
 	
-@Test
-public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
-	int value = max.findMax(9, 5, 6);
-	Assert.assertEquals(9, value);
-}
 
-@Test
-public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
-	int value = max.findMax(5, 9, 6);
-	Assert.assertEquals(9, value);
-}
+	@Test
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtFirstPosition() {
+		int value = new Generics<Integer>(9, 5, 4).findMax();
+		Assert.assertEquals(9, value);
+	}
 
-@Test
-public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
-	int value = max.findMax(6, 5, 9);
-	Assert.assertEquals(9, value);
-}
+	@Test
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtSecondPosition() {
+		int value = new Generics<Integer>(5, 9, 6).findMax();
+		Assert.assertEquals(9, value);
+	}
 
-@Test
-public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
-	float value = max.findMax(9.0f, 5.0f, 6.0f);
-	Assert.assertEquals(9.0f, value, 0.0f);
-}
+	@Test
+	public void givenThreeNumber_WhenIntegerNumber_ShouldReturnMaxAtThirdPosition() {
+		int value = new Generics<Integer>(6, 5, 9).findMax();
+		Assert.assertEquals(9, value);
+	}
 
-@Test
-public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
-	float value = max.findMax(5.0f, 9.0f, 6.0f);
-	Assert.assertEquals(9.0f, value, 0.0f);
-}
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxAtFirstPosition() {
+		float value = new Generics<Float>(9.0f, 5.0f, 6.0f).findMax();
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
 
-@Test
-public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
-	float value = max.findMax(9.0f, 5.0f, 11.0f);
-	Assert.assertEquals(11.0f, value, 0.0f);
-}
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxSecoundPosition() {
+		float value = new Generics<Float>(5.0f, 9.0f, 6.0f).findMax();
+		Assert.assertEquals(9.0f, value, 0.0f);
+	}
 
-@Test
-public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtFirstPosition() {
-	String value = max.findMax("peach", "apple", "banana");
-	Assert.assertEquals("peach", value);
-}
+	@Test
+	public void givenThreeNumber_WhenFloatNumber_ShouldReturnMaxThirdPosition() {
+		float value = new Generics<Float>(9.0f, 5.0f, 11.0f).findMax();
+		Assert.assertEquals(11.0f, value, 0.0f);
+	}
 
-@Test
-public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtSecoundPosition() {
-	String value = max.findMax("apple", "peach", "banana");
-	Assert.assertEquals("peach", value);
-}
+	@Test
+	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtFirstPosition() {
+		String value = new Generics<String>("peach", "apple", "banana").findMax();
+		Assert.assertEquals("peach", value);
+	}
 
-@Test
-public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtThirdPosition() {
-	String value = max.findMax("apple", "banana", "peach");
-	Assert.assertEquals("peach", value);
-}
+	@Test
+	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtSecoundPosition() {
+		String value = new Generics<String>("apple", "peach", "banana").findMax();
+		Assert.assertEquals("peach", value);
+	}
 
+	@Test
+	public void givenThreeNumber_WhenStringValue_ShouldReturnMaxAtThirdPosition() {
+		String value = new Generics<String>("apple", "banana", "peach").findMax();
+		Assert.assertEquals("peach", value);
+	}
 
- 
 }
